ae29af4a1c57a94792a4da68a7483c0348363e40
ScStew <scottmstewart2@gmail.com>
2026-05-24 22:09:38 -0400

    added basic login routing and form

--- backend/app.rb
+++ backend/app.rb
@@ -12,23 +12,65 @@ require_relative './config/database.rb'
 
 SECRET_KEY = "ENV['SECRET_KEY']"
 
+# Basic route to test if the server is running
 get '/' do
     'Hello World'
 end
 
+# Test route to verify backend is working
+    get "/api/test" do
+    content_type :json
 
-get "/api/test" do
+    {
+        message: "Sinatra backend connected successfully!"
+    }.to_json
+    end
+
+    get "/db-test" do
+    ActiveRecord::Base.connection.active?
+
+    {
+        connected: true
+    }.to_json
+    end
+# Add more routes and logic here for your portfolio site backend
+
+
+# Temporary login route for testing JWT authentication
+post "/api/auth/login" do
   content_type :json
 
-  {
-    message: "Sinatra backend connected successfully!"
-  }.to_json
-end
+  body = JSON.parse(request.body.read)
 
-get "/db-test" do
-  ActiveRecord::Base.connection.active?
+  email = body["email"]
+  password = body["password"]
+
+  # TEMP fake user
+  user = {
+    id: 1,
+    email: "admin@test.com",
+    password_digest: BCrypt::Password.create("password123"),
+    role: "admin"
+  }
+
+  if email != user[:email]
+    halt 401, { error: "Invalid credentials" }.to_json
+  end
+
+  if !BCrypt::Password.new(user[:password_digest]) == password
+    halt 401, { error: "Invalid credentials" }.to_json
+  end
+
+  token = JWT.encode(
+    {
+      user_id: user[:id],
+      role: user[:role]
+    },
+    SECRET_KEY,
+    "HS256"
+  )
 
   {
-    connected: true
+    token: token
   }.to_json
 end
